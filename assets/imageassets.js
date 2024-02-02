782ceecce419ade23f78d4c99b8d91d804c47a0c
nitinco <sharmanitin96644@gmail.com>
2024-02-02 10:39:16 +0530

    awesome

--- assets/imageassets.js
+++ assets/imageassets.js
@@ -1,5 +1,8 @@
 const ImagesAssets ={
     bannerlist1:require('../assets/test.jpg'),
     bannerlist2:require('../assets/test.jpg'),
+    Nitin:require('../assets/nitin.jpg'),
+    Ashish:require('../assets/ashish.jpg'),
+    sadhaya:require('../assets/sadhaya.jpg'),
 };
 export default ImagesAssets;
--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -1,5 +1,6 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ImagesAssets from '../assets/imageassets';
 
 export default function ContactList() {
     const contacts = [
@@ -7,24 +8,32 @@ export default function ContactList() {
             uid:1,
             name:'nitin sharma',
             status:'i am a software developer',
+            imageurl:ImagesAssets.Nitin,
         },
         {
             uid:2,
             name:'Ashish Verma',
             status:'i am a Backend Expert',
+            imageurl:ImagesAssets.Ashish,
         },
         {
             uid:3,
             name:'Sadhya dashora',
             status:'i am a Frontend developer',
+            imageurl:ImagesAssets.sadhaya,
         }
     ];
   return (
     <View >
       <Text style={styles.headingtext}>ContactList</Text>
       <ScrollView style={styles.container} scrollEnabled={false}>
-        {contacts.map((uid,name,status)=>(
-            <View ></View>
+        {contacts.map(({uid,name,status,imageurl})=>(
+            <View key={uid}>
+              <Image source={imageurl} style={styles.userimage} />
+              <Text>{name}</Text>
+              <Text>{status}</Text>
+
+            </View>
         ))}
 
       </ScrollView>
@@ -41,7 +50,15 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10
       },
       container:{
+        // justifyContent:'center',
 
+      },
+      userimage:{
+        paddingHorizontal:100,
+        borderRadius:9,
+        
+        width:200,
+        height:400
       },
 
 })
